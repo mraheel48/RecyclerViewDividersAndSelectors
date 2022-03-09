5adbae08bff4a2a1b21dd0cb8fefb0bdaf5b5407
mraheeldev <m.raheeldev@gmail.com>
2022-03-09 14:36:31 +0500

    Initial commit

---
+++ app/src/main/java/com/raheel/recyclerviewdividersandselectors/Model.java
@@ -0,0 +1,12 @@
+package com.raheel.recyclerviewdividersandselectors;
+
+public class Model {
+
+    String text;
+    boolean colored;
+
+    public Model(String text, boolean colored) {
+        this.text = text;
+        this.colored = colored;
+    }
+}
